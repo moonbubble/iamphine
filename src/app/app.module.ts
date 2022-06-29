c3ab5a52fb11a42d3d96f2e27101011f28db9bb2
Ralphine <r.deroo99@gmail.com>
2022-06-29 15:56:44 +0200

    Adds homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PaperComponent } from './components/paper/paper.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PaperComponent],
+  declarations: [AppComponent, HomeComponent, PaperComponent, NavigationComponent, SectionComponent],
   imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+    },
     extend: {
+      screens: {
+        '3xl':  '1792px'
+      },
       fontFamily: {
-        'vidaloka': ['"Vidaloka"', 'serif']
+        'vidaloka': ['"Vidaloka"', 'serif'],
+        'caveat': ['"Caveat"', 'cursive'],
       }
     },
   },
